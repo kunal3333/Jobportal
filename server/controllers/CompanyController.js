40a8bde29594448471150358fd6afc79588e95bd
kunal3333 <63576112+kunal3333@users.noreply.github.com>
2025-04-15 12:52:12 +0530

    backend

---
+++ server/controllers/CompanyController.js
@@ -0,0 +1,42 @@
+// register a new company
+export const registerCompnay = async (req,res) => {
+    
+}
+
+//company Login
+ export const loginCompany = async (req,res) => {
+    
+}
+
+
+// get company data
+export const getCompanyData = async (req,res) => {
+    
+}
+
+
+// Post a new Job 
+export const postjob = async (req,res) => {
+    
+}
+
+// get company Applications
+
+export const getCompanyjobApplications = async (req,res) => {
+    
+}
+
+// get compnay posted Jobs
+export const getCompanyPostedJobs = async (req,res) => {
+    
+}
+
+
+//change Job Application Status
+export const ChangeJobApplicationStatus = async (req,res) => {
+    
+}
+
+// change job visibility
+
+export
